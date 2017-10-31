6d0760be7dcefc5d7fb5b83184e75cf20c938b81
reganartinfo <rachel.fisher.egan@gmail.com>
2017-10-31 10:57:38 -0400

    added comments

--- hopper_dates.py
+++ hopper_dates.py
@@ -1,28 +1,44 @@
 import json, re
 
+# set arrays for dates
 range_dates = []
 no_dates = []
 other_dates = []
+# pythex_date = regex for year
 pythex_date = re.compile("\d{4}")
 
+# opens hopper_dump.json written by cds_request.py
 with open("hopper_dump.json","r") as f:
 	encoder = json.load(f)
+
+	# loops through each json object in hopper_dump + by display date value
 	for an_object in encoder:
 		object_date = an_object["display_date"]
+
+		# checks for an object with no date ("n.d.") + appends object to no_dates array
 		if object_date == "n.d.":
 			no_dates.append(an_object)
+
+		# checks for an object with dates
 		else:
 			regex_date = pythex_date.findall(object_date)
 			earliest_date = int(regex_date[0])
+
+			# appends object that fall into range (1925-1937, inclusive) to range_dates array 
 			if (earliest_date >= 1925) and (earliest_date <= 1937):
 				range_dates.append(an_object)
+
+			# appends object that falls outside range to other_dates array
 			else:
 				other_dates.append(an_object)
 
+# status update
 print("Count of objects with no dates: " + str(len(no_dates)))
 print("Count of objects within date range: " + str(len(range_dates)))
 print("Count of objects outside of date range: " + str(len(other_dates)))
 
+# stores sorted json results for later with pretty print
+# hopper_dates.json = the goods!
 json.dump(no_dates,open("hopper_nodates.json","w"),indent=4)
 json.dump(range_dates,open("hopper_dates.json","w"),indent=4)
 json.dump(other_dates,open("hopper_otherdates.json","w"),indent=4)
