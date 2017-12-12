cd101edbd81d60f4dde075a585236f549de7f3d3
reganartinfo <rachel.fisher.egan@gmail.com>
2017-12-12 10:53:57 -0500

    more directory cleaning

--- scripts/cds_request.py
+++ scripts/cds_request.py
@@ -11,22 +11,23 @@ data = json.loads(r.text)
 # total_count = total # of results that match query
 total_count = data["count"]
 print(total_count)
+
 # CDS API returns 1 page with 36 results per request --> params cannot be changed :(
-# page_count = round(total_count/36)
-# # initialize page param
-# page = 1
-# # set array for results
-# hopper_objects = []
+page_count = round(total_count/36)
+# initialize page param
+page = 1
+# set array for results
+hopper_objects = []
 
-# # while loop makes request + increments page # until all results are returned
-# while page <= page_count:
-# 	print("Requesting page " + str(page) + " . . .")
-# 	query = {"format":"json", "artist_id":"621", "page":page}
-# 	r = requests.get("http://collection.whitney.org/json/objects", params=query)
-# 	data = json.loads(r.text)
-# 	for an_object in data["results"]:
-# 		hopper_objects.append(an_object)
-# 	page += 1
+# while loop makes request + increments page # until all results are returned
+while page <= page_count:
+	print("Requesting page " + str(page) + " . . .")
+	query = {"format":"json", "artist_id":"621", "page":page}
+	r = requests.get("http://collection.whitney.org/json/objects", params=query)
+	data = json.loads(r.text)
+	for an_object in data["results"]:
+		hopper_objects.append(an_object)
+	page += 1
 
-# # stores json results for later with pretty print
-# json.dump(hopper_objects,open("hopper_dump.json","w"), indent=4)
+# stores json results for later with pretty print
+json.dump(hopper_objects,open("hopper_dump.json","w"), indent=4)
